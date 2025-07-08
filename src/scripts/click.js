4536fbc3c69628053f1a6a9ea767ac7c561ad3cf
Ryan <ranli.ryan@gmail.com>
2025-07-08 13:23:39 -0700

    Update click.js

--- src/scripts/click.js
+++ src/scripts/click.js
@@ -1,1 +1,21 @@
+function test() {
+  const foundCheckbox = document.querySelector(".recaptcha-checkbox");
+  if (foundCheckbox) {
+    foundCheckbox.click();
+  }
+  console.log(foundCheckbox);
+  const solveBtn = document.querySelector('#solver-button');
+  if (solveBtn) {
+    solveBtn.click();
+  }
+  console.log(solveBtn);
+}
 
+window.addEventListener('DOMContentLoaded', () => {
+    const observer = new MutationObserver(test);
+    observer.observe(document, {
+      childList: true,
+      subtree: true
+    });
+    test();
+});
